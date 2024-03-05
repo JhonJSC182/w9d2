1ef3b79f62f377083176d99952e23ea734ad2376
JhonJSC182 <jhonjsc182@hotmail.com>
2024-03-05 13:52:00 -0800

    add things on index and view

--- src/ttt-view.js
+++ src/ttt-view.js
@@ -1,8 +1,17 @@
 class View {
   constructor(game, el) {
+    this.setupBoard();
   }
   
   setupBoard() {
+    const boardSize = this.game.board.grid.length;
+    const board = document.createElement('ul');
+
+    for (let row = 0; row < boardSize; row++) {
+      for (let column = 0; column < boardSize; column++) {
+        
+      }
+    }
   }
   
   handleClick(e) {
